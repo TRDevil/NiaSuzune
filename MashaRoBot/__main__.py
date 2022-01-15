a332bfb653c21d8124a320765448890276d22b03
TR Crazy ✠ <91002607+TRDevil@users.noreply.github.com>
2022-01-15 20:18:22 +0530

    `Done`

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -122,14 +122,8 @@ MIKU_IMG = (
 PM_PHOTO = (
       "https://telegra.ph/file/516a9d6395248a5f5ced7.jpg",
       "https://telegra.ph/file/a2188d060cf13f116bef5.jpg",
-
-TEXXT = (
-      "*Hey* [{}](tg://settings/), *As you know I'm alive since:* `{}`", 
-      "*Hey* [{}](tg://settings/), *I'm came for you from* `{}` \n*Do you Like me?*", 
-      "*Hey* [{}](tg://settings/) *I'm here for you from* `{}`", 
-      "*Hey* [{}](tg://settings/), *I'm alive From:* `{}`",
-      "*Yes Darling* [{}](tg://settings/), *I'm alive Since:* `{}`",
-)
+  
+TEXXT = ( "*Hey* [{}](tg://settings/), *As you know I'm alive since:* `{}`", "*Hey* [{}](tg://settings/), *I'm came for you from* `{}`" )
 
 MIKU_N_IMG = (
       "https://telegra.ph/file/516a9d6395248a5f5ced7.jpg",
