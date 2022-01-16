5bdc850a0a6d3c2aa10146e034bfb31cc6badaeb
TR Crazy ✠ <91002607+TRDevil@users.noreply.github.com>
2022-01-16 08:06:19 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -228,10 +228,6 @@ def start(update: Update, context: CallbackContext):
             update.effective_message.reply_photo(
                 MIKU_N_IMG,
                 PM_START_TEXT.format(
-                    escape_markdown(first_name),
-                    escape_markdown(uptime),
-                    sql.num_users(),
-                    sql.num_chats()),                        
                 reply_markup=InlineKeyboardMarkup(buttons),
                 parse_mode=ParseMode.MARKDOWN,
                 timeout=5,
