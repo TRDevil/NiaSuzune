915fd109d6dcf51b0b8489d549d82fb5d24a87da
TR Crazy ✠ <91002607+TRDevil@users.noreply.github.com>
2022-01-16 12:08:50 +0530

    Update sudoers.py

--- MashaRoBot/modules/sudoers.py
+++ MashaRoBot/modules/sudoers.py
@@ -6,7 +6,7 @@ import time
 import psutil
 from pyrogram import filters
 
-from MashaRoBot import (StartTime, DEV_USERS, pgram)
+from MashaRoBot import (StartTime, DEV_USERS, pbot)
 import MashaRoBot.utils.formatter as formatter
 import MashaRoBot.modules.sql.users_sql as sql
 
